a70e24dc80c673e6657068273cb0af809519c591
Rachneet <rachneetsethi@gmail.com>
2019-02-24 05:19:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,16 @@ speak_to_grandma = "can you hear me?"
 if speak_to_grandma = speak_to_grandma.lowcase 
   puts "HUH?! SPEAK UP SONNY!"
   
+elsif speak_to_grandma = speak_to_grandma.upcase
+  puts "CAN YOU HEAR ME?"
+  
+elsif speak_to_grandma = speak_to_grandma.upcase
+puts "NO, NOT SINCE 1938"
 
+elsif speak_to_grandma = speak_to_grandma.upcase 
+puts "I LOVE YOU GRANDMA!"
+
+else 
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
