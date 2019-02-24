b2dac1919a68f2120d9bb15d68d9158ef1f8c9d0
Rachneet <rachneetsethi@gmail.com>
2019-02-24 05:20:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,10 @@
-speak_to_grandma = "can you hear me?"
-  
-if speak_to_grandma = speak_to_grandma.lowcase 
-  puts "HUH?! SPEAK UP SONNY!"
-  
-elsif speak_to_grandma = speak_to_grandma.upcase
-  puts "CAN YOU HEAR ME?"
-  
-elsif speak_to_grandma = speak_to_grandma.upcase
-puts "NO, NOT SINCE 1938"
-
-elsif speak_to_grandma = speak_to_grandma.upcase 
-puts "I LOVE YOU GRANDMA!"
-
-else 
-  puts "I LOVE YOU TOO PUMPKIN"
-
+def speak_to_grandma(phrase)
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  end
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
 end
